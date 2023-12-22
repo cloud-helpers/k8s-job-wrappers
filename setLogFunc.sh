82179e855fbeffa2a54372d1c1784fc47adb9ed2
Denis Arnaud <denis.arnaud@decathlon.com>
2023-12-22 11:06:28 +0100

    [Logs] Fixed a typo when reporting the calling script

--- setLogFunc.sh
+++ setLogFunc.sh
@@ -2,9 +2,14 @@
 #
 # File: https://github.com/cloud-helpers/k8s-job-wrappers/tree/master/setLogFunc.sh
 #
-# Utility supporting executing tasks from within Kubernetes (k8s) pods
+# Utility supporting generating log files for off-line/batched jobs,
+# for instance when executed from within Kubernetes (k8s) pods
 #
-# Logging support functions
+# The following variables should be specified by the caller script:
+# * KJW_LOG_FILE: the (Unix) path of the file where the logs will be written.
+#                 That file must be writeable by the calling user
+# * KJW_SCRIPT_URL: the URL or the file-path of the calling script. That is for
+#                   information, very useful when referenced from the logs
 #
 # Dependencies:
 # * setDistAndArch.sh (https://github.com/cloud-helpers/k8s-job-wrappers/tree/master/setDistAndArch.sh)
@@ -42,27 +47,27 @@ fi
 log() {
     if [ -z "${KJW_LOG_FILE}" ]
     then
-	echo "Error - The KJW_LOG_FILE environment variable should be set and points to the file in which the logs may be written to" > /dev/stderr
-	echo "        See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."  > /dev/stderr
-	return 1
+		echo "Error - The KJW_LOG_FILE environment variable should be set and points to the file in which the logs may be written to" > /dev/stderr
+		echo "        See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."  > /dev/stderr
+		return 1
     fi
 
     local canWriteToFile="$(touch ${KJW_LOG_FILE} 2> /dev/null && echo "Y" || echo "N")"
     if [ "${canWriteToFile}" == "N" ]
     then
-	echo "Error - The ${KJW_LOG_FILE} is expected to be a wrtiteable file in which the logs may be written; apparently, no logs may be written to ${KJW_LOG_FILE}."  > /dev/stderr
-	echo "        See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."  > /dev/stderr
-	return 1
+		echo "Error - The ${KJW_LOG_FILE} is expected to be a wrtiteable file in which the logs may be written; apparently, no logs may be written to ${KJW_LOG_FILE}."  > /dev/stderr
+		echo "        See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."  > /dev/stderr
+		return 1
     fi
 
     if [ -z "${KJW_SCRIPT_URL}" ]
     then
-	echo "Error - The KJW_SCRIPT_URL environment variable should be set, but is not."
-	echo "See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."
-	return 1
+		echo "Error - The KJW_SCRIPT_URL environment variable should be set, but is not."
+		echo "See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."
+		return 1
     fi
 
-    local caller_script="$(basename ${THIS_KJW_SCRIPT_URL})"
+    local caller_script="$(basename ${KJW_SCRIPT_URL})"
 
     logTime="$(${DATE_TOOL} "+%Y-%m-%d %H:%M:%S" --utc)"
 
@@ -72,27 +77,27 @@ log() {
 logMulti() {
     if [ -z "${KJW_LOG_FILE}" ]
     then
-	echo "Error - The KJW_LOG_FILE environment variable should be set and points to the file in which the logs may be written." > /dev/stderr
-	echo "        See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."  > /dev/stderr
-	return 1
+		echo "Error - The KJW_LOG_FILE environment variable should be set and points to the file in which the logs may be written." > /dev/stderr
+		echo "        See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."  > /dev/stderr
+		return 1
     fi
 
     local canWriteToFile="$(touch ${KJW_LOG_FILE} 2> /dev/null && echo "Y" || echo "N")"
     if [ "${canWriteToFile}" == "N" ]
     then
-	echo "Error - The ${KJW_LOG_FILE} is expected to be a wrtiteable file in which the logs may be written; apparently, no logs may be written to ${KJW_LOG_FILE}."  > /dev/stderr
-	echo "        See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."  > /dev/stderr
-	return 1
+		echo "Error - The ${KJW_LOG_FILE} is expected to be a wrtiteable file in which the logs may be written; apparently, no logs may be written to ${KJW_LOG_FILE}."  > /dev/stderr
+		echo "        See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."  > /dev/stderr
+		return 1
     fi
 
     if [ -z "${KJW_SCRIPT_URL}" ]
     then
-	echo "Error - The KJW_SCRIPT_URL environment variable should be set, but is not."
-	echo "See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."
-	return 1
+		echo "Error - The KJW_SCRIPT_URL environment variable should be set, but is not."
+		echo "See ${THIS_KJW_SCRIPT_URL} for the details of the log() function."
+		return 1
     fi
 
-    local caller_script="$(basename ${THIS_KJW_SCRIPT_URL})"
+    local caller_script="$(basename ${KJW_SCRIPT_URL})"
 
     logTime="$(${DATE_TOOL} "+%Y-%m-%d %H:%M:%S" --utc)"
 
@@ -101,7 +106,7 @@ logMulti() {
     #declare -a line_list=($@)
     for myline in "$@"
     do
-	echo "${myline}" | tee -a ${KJW_LOG_FILE}
+		echo "${myline}" | tee -a ${KJW_LOG_FILE}
     done
 
     echo "############### [${logTime} (UTC)][${KJW_UNAME}@${KJW_HNAME}][${KJW_FUNC}] - end ######################" | tee -a ${KJW_LOG_FILE}
@@ -111,7 +116,7 @@ logStart() {
     local descr="Generic Description - Place your own description here by calling logStart() with an argument"
     if [ ! -z "$1" ]
     then
-	descr="$1"
+		descr="$1"
     fi
     log "#############################################################################"
     log "#### ${descr} - Start ####"
@@ -121,7 +126,7 @@ logEnd() {
     local descr="Generic Description - Place your own description here by calling logStart() with an argument"
     if [ ! -z "$1" ]
     then
-	descr="$1"
+		descr="$1"
     fi
     log "#### ${descr} - End ####"
     log "#############################################################################"
